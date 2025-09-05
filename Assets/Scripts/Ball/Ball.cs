89823de522f64ec697f51f15157337f93801b501
Reid W <reiddwilliamson@gmail.com>
2025-09-05 13:47:07 -0700

    fixed scoring boundary

--- Assets/Scripts/Ball/Ball.cs
+++ Assets/Scripts/Ball/Ball.cs
@@ -20,31 +20,31 @@ public class Ball : MonoBehaviour
     private void FixedUpdate()
     {
 
-        float screenTop = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0)).y;
-        float screenBottom = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
+        Vector3 screenTop = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0));
+        Vector3 screenBottom = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
         Vector2 pos = rb.position + speed * dir * Time.fixedDeltaTime;
 
         // check if a point is scored
-        if(pos.x < screenBottom)
+        if(pos.x < screenBottom.x + sr.bounds.extents.x)
         {
             ScoreManager.Instance.AddPoint(1);
-        } else if (pos.x > screenTop)
+        } else if (pos.x > screenTop.x - sr.bounds.extents.x)
         {
             ScoreManager.Instance.AddPoint(0);
         }
 
-        if (pos.y < screenBottom + sr.bounds.extents.y)
+        if (pos.y < screenBottom.y + sr.bounds.extents.y)
         {
             dir = dir - 2 * Vector2.Dot(dir, Vector2.up) * Vector2.up;
             dir.Normalize();
         }
-        else if (pos.y > screenTop - sr.bounds.extents.y)
+        else if (pos.y > screenTop.y - sr.bounds.extents.y)
         {
             dir = dir - 2 * Vector2.Dot(dir, Vector2.down) * Vector2.down;
             dir.Normalize();
         }
 
-        pos.y = Mathf.Clamp(pos.y, screenBottom + sr.bounds.extents.y, screenTop - sr.bounds.extents.y);
+        pos.y = Mathf.Clamp(pos.y, screenBottom.y + sr.bounds.extents.y, screenTop.y - sr.bounds.extents.y);
         rb.MovePosition(pos);
     }
 
